f40783625969b8684f645a7863385b12ae4ccce1
Disturbedcobra <46019798+Disturbedcobra@users.noreply.github.com>
2024-03-28 11:08:10 +0200

    Create palvelinten_hallinta_H1.mk

---
+++ palvelinten_hallinta_H1.mk
@@ -0,0 +1,13 @@
+# Tehtävä X
+
+##    Salt
+    
+- Salt komentoja voi suorittaa lokaalisti ja nähdä tulokset välittömästi.
+
+- samat Salt komennot toimivat Windows- sekä Linux:illa.
+
+- tärkeimmät tila funktiot ovat pkg, file, service, user sekä cmd.
+
+- Salt:ia käytetään yleensä kontrolloimaan suuria määriä orja tietokoneita internetin välityksellä.
+
+    
